2d292ef11d8d233d5520931cd05c3b002404afd3
André R.B. (mac) <bronca.andre@gmail.com>
2022-08-25 17:32:20 -0300

    criando um ambiente de teste

---
+++ chapter2/src/main/java/com/bsg5/chapter2/HelloWorldGreeter.java
@@ -0,0 +1,20 @@
+package com.bsg5.chapter2;
+
+import java.io.PrintStream;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldGreeter implements Greeter{
+
+    @Override
+    public void setPrintStream(PrintStream printStream) {
+        
+    }
+
+    @Override
+    public void greet() {
+        
+    }
+    
+}
